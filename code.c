bbe31734472642a7bad14f0f9bbc968227dc1ac5
williampariwat <william.pariwat@gmail.com>
2021-05-10 22:52:32 +0700

    Fix the git tag bug

--- code.c
+++ code.c
@@ -1,10 +1,15 @@
+// #Pariwat Huang 6180067
+// Reference : 
+// 			https://github.com/szholdiyarov/command-line-interpreter/blob/master/myshell.c
+//			https://stackoverflow.com/questions/58361506/save-history-command-on-simple-shell-by-c-code
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>   // Definition for fork() and execve()
 #include <errno.h>    // Definition for "error handling"
 #include <sys/wait.h> // Definition for wait()
-
+#define MAXCHAR 1000
 	/* Declarations for getline() */
 	char *input = NULL;
 	size_t capline = 0; // Capacity
@@ -12,6 +17,8 @@
 	/* Declaration for strtok() */
 	int i;
 
+	//Declaration for file
+	FILE *fp;
 
 
 	// count numbers of indices in prev array
@@ -23,6 +30,19 @@
 	char *prev[512]; 
 
 
+
+	// void checkForFile(char const *argv[])
+	// {
+	// 	char str[MAXCHAR];
+		
+	// 	fp = fopen(argv[1], "r");
+	// 	// if (fp == NULL){
+	// 	//     printf("Could not open file %s",argv[1]);
+	// 	// }
+	// 	while (fgets(str, MAXCHAR, fp) != NULL)
+	// 	    printf("%s", str);
+	// 	fclose(fp);
+	// }
 
    void printArr(char** array){
    		for(int i = 0; i < sizeof(array); i++){
@@ -47,17 +67,11 @@
 		i = 0;
 		// idx = 0
 		token = strtok(input, "\n ");
-		while (token != NULL) { 
-			// count++;
-
-			if(strcmp(token,"!!") == 0) {
-				continue;
-			}else{
+			while (token != NULL) { 
 				count++;
+				array[i++] = token; // Add tokens into the array
+				token = strtok(NULL, "\n ");
 			}
-			array[i++] = token; // Add tokens into the array
-			token = strtok(NULL, "\n ");
-		}
 		array[i] = NULL;
 	}
 
@@ -74,7 +88,7 @@
 			}				
 		}
 		// reset count to zero
-		// count = 0;
+		count = 0;
    }
 
    void copyArr(char** des, char** src){
@@ -114,32 +128,50 @@
 
 	
 	int main(int argc, char const *argv[]){
-		startDisplay();
+
+		// if(argv[1] != NULL){
+		// 	char str[MAXCHAR];
+			
+		// 	fp = fopen(argv[1], "r");
+		// 	// if (fp == NULL){
+		// 	//     printf("Could not open file %s",argv[1]);
+		// 	// }
+		// 	while (fgets(str, MAXCHAR, fp) != NULL){
+		// 	    printf("Print: %s \n", str);
+		// 		makeTokens(str);
+		// 		checkBangs(array,prev);
+		// 		execute();
+		// 	}
+		// 	fclose(fp);		
+		// }
+
+		// else{
+			startDisplay();
 
 
-		while(1){
-			displayPrompt(); // Display a user prompt
-			getline(&input, &capline, stdin); // Read the user input
-			/* Check if input is empty */
-			if(strcmp(input,"\n")==0){
-				continue;
+			while(1){
+				displayPrompt(); // Display a user prompt
+				getline(&input, &capline, stdin); // Read the user input
+				/* Check if input is empty */
+				if(strcmp(input,"\n")==0){
+					continue;
+				}
+			
+				makeTokens(input); // Divide line into tokens 
+
+	            /* Check if input is "q", if yes then exit shell */
+				if (strcmp(array[0], "exit") == 0 && strcmp(array[1], "1") == 0) {
+					printf("bye \n");
+					return 0;
+				}
+
+				checkBangs(array,prev);
+				// printArr(prev);
+
+				execute(); // Call execvp()
+
 			}
-		
-			makeTokens(input); // Divide line into tokens 
-
-            /* Check if input is "q", if yes then exit shell */
-			// if (strcmp(array[0], "exit") == 0 && strcmp(array[1], "1") == 0) {
-			// 	printf("bye \n");
-			// 	return 0;
-			// }
-			// printf("History \n");
-			// printArr(prev);
-			checkBangs(array,prev);
-			// printArr(prev);
-
-			execute(); // Call execvp()
-
-	}
+		}
 	
 	
-}
+// }
